9813578b813e52f57edb9fc17c538ce5d9cab1da
mxdlance <48517626+mxdlance@users.noreply.github.com>
2019-03-26 19:02:56 +0800

    首次提交

---
+++ src/main/java/com/gdkm/weixin/微信公众号Application.java
@@ -0,0 +1,13 @@
+package com.gdkm.weixin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class 微信公众号Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(微信公众号Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdkm/weixin/微信公众号ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gdkm.weixin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class 微信公众号ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
